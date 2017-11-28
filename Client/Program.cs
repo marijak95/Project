5b8de38197772be52189a9873553c59d5f8fa1bc
jelacic994 <jelacic994@gmail.com>
2017-11-28 20:16:13 +0100

    client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Common;
+using System;
+using System.ServiceModel;
+using System.Threading;
 
 namespace Client
 {
@@ -10,6 +9,40 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            UserInterface userInterface = new UserInterface();
+            ClientAccount clientAccount = new ClientAccount();
+            ConnectionHandler connectionHandler = new ConnectionHandler();
+            int userCommand;
+
+            IWorkgroupService workgroupProxy = null; //Null as we're creating it upon login...
+            IRegistrationService registrationProxy = connectionHandler.registrationChannel.CreateChannel();
+
+            do
+            {
+                userCommand = userInterface.AnonymousUserInterface(ref workgroupProxy, registrationProxy, connectionHandler, clientAccount);
+                if (userCommand == 0)
+                {
+                    connectionHandler.CloseRegistrationChannel();
+                    connectionHandler.CloseWorkgroupChannel();
+                    return;
+                }
+            } while (!clientAccount.Authenticated);
+
+            Console.WriteLine("Succesffully authenticated!");
+
+            connectionHandler.CloseRegistrationChannel(); //Registration channel is no longer needed, close it.
+            registrationProxy = null;
+
+            if (clientAccount.ClientGroup == "Administrator") //Napravite enumeraciju za Grupe u Common biblioteci...
+            {
+                userInterface.AdminWindow(clientAccount, workgroupProxy);
+            }
+            else //User ? Boss? Director? 
+            {
+                userInterface.WorkerWindow(clientAccount, workgroupProxy, connectionHandler);
+            }
+
+            Console.ReadLine();
         }
     }
 }
